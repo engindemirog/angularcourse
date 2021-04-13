b7ff25ef38b530c512363e9791d4fe67795af745
Engin Demiroğ <engindemirog@gmail.com>
2021-04-13 10:43:53 +0300

    13 Nisan Commit 2

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { CartItems } from '../appStates/cartItemState';
+import { CartItem } from '../models/cartItem';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  addToCart(product:Product){
+    let item = CartItems.find(c=>c.product.id === product.id)
+    if(item){
+      item.quantity++;
+    }else{
+      let cartItem = new CartItem()
+      cartItem.quantity = 1;
+      cartItem.product = product;
+      CartItems.push(cartItem)
+    }
+  }
+
+  removeFromCart(product:Product){
+    let item = CartItems.find(c=>c.product.id === product.id)
+    if(item){
+      CartItems.splice(CartItems.indexOf(item),1)
+    }
+  }
+
+  list():CartItem[]{
+    return CartItems;
+  }
+}
